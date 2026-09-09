e322692a90d05ffb9ef45e4eb2033197df4f91c9
jihvijhojhviihogyuvi <wolfe.charlie.r@gmail.com>
2026-09-09 18:32:00 -0400

    Transmit timestamps with messages

--- src/Services/LoRaPacket.cpp
+++ src/Services/LoRaPacket.cpp
@@ -1,11 +1,12 @@
 #include "LoRaPacket.h"
 
 size_t MessagePacket::pack(void** destination) const{
-	size_t size = sizeof(uid) + sizeof(type);
+	size_t size = sizeof(uid) + sizeof(type) + sizeof(timestamp);
 	uint8_t* buffer = static_cast<uint8_t*>(malloc(size));
 
 	memcpy(buffer, &type, sizeof(type));
 	memcpy(buffer + sizeof(type), &uid, sizeof(uid));
+	memcpy(buffer + sizeof(type) + sizeof(uid), &timestamp, sizeof(timestamp));
 
 	*destination = buffer;
 	return size;
@@ -21,21 +22,23 @@ MessagePacket* MessagePacket::unpack(void* _buffer){
 		packet = new MessagePacket;
 		packet->type = type;
 	}else if(type == TEXT){
-		packet = TextMessage::unpack(buffer + sizeof(type) + sizeof(uid));
+		packet = TextMessage::unpack(buffer + sizeof(type) + sizeof(uid) + sizeof(timestamp));
 	}else if(type == PIC){
-		packet = PicMessage::unpack(buffer + sizeof(type) + sizeof(uid));
+		packet = PicMessage::unpack(buffer + sizeof(type) + sizeof(uid) + sizeof(timestamp));
 	}else{
 		return nullptr;
 	}
 
 	packet->type = type;
 	packet->uid = *reinterpret_cast<UID_t*>(buffer + sizeof(Type));
+	packet->timestamp = *reinterpret_cast<uint32_t*>(buffer + sizeof(Type) + sizeof(UID_t));
 
 	return packet;
 }
 
 TextMessage::TextMessage(){
 	type = TEXT;
+	timestamp = 0;
 }
 
 TextMessage::TextMessage(const std::string &text) : TextMessage(){
@@ -69,6 +72,7 @@ TextMessage* TextMessage::unpack(void* _buffer){
 
 PicMessage::PicMessage(){
 	type = PIC;
+	timestamp = 0;
 }
 
 PicMessage::PicMessage(uint8_t index) : PicMessage(){
@@ -126,69 +130,4 @@ size_t RequestPair::pack(void** destination) const{
 
 	*destination = buffer;
 	return sizeof(encKey);
-}
-
-AckPair* AckPair::unpack(void* buffer){
-	auto pairPacket = new AckPair;
-	memcpy(pairPacket->encKey, buffer, sizeof(encKey));
-	return pairPacket;
-}
-
-size_t AckPair::pack(void** destination) const{
-	uint8_t* buffer;
-
-	buffer = static_cast<uint8_t*>(realloc(buffer, sizeof(encKey)));
-	memcpy(buffer, &encKey, sizeof(encKey));
-
-	*destination = buffer;
-	return sizeof(encKey);
-}
-
-size_t ProfilePacket::pack(void** destination) const{
-	auto buffer = static_cast<uint8_t*>(malloc(sizeof(type)));
-	memcpy(buffer, &type, sizeof(type));
-	*destination = buffer;
-	return sizeof(type);
-}
-
-ProfilePacket* ProfilePacket::unpack(void* _buffer){
-	auto buffer = static_cast<uint8_t*>(_buffer);
-
-	ProfilePacket* packet;
-	Type type = *reinterpret_cast<Type*>(buffer);
-
-	if(type == RESP){
-		packet = ProfileResponse::unpack(buffer + sizeof(Type));
-	}else if(type == REQ){
-		packet = new ProfilePacket;
-	}else{
-		return nullptr;
-	}
-	packet->type = type;
-	return packet;
-}
-
-size_t ProfileResponse::pack(void** destination) const{
-	uint8_t* buffer;
-	size_t size = ProfilePacket::pack(reinterpret_cast<void**>(&buffer));
-
-	buffer = static_cast<uint8_t*>(realloc(buffer, size + sizeof(Profile)));
-	memcpy(buffer + size, &profile, sizeof(Profile));
-
-	*destination = buffer;
-	return size + sizeof(Profile);
-}
-
-ProfileResponse::ProfileResponse(){
-	type = RESP;
-}
-
-ProfileResponse::ProfileResponse(const Profile &prof) : ProfileResponse(){
-	profile = prof;
-}
-
-ProfileResponse* ProfileResponse::unpack(void* buffer){
-	ProfileResponse* profile = new ProfileResponse;
-	profile->profile = *static_cast<Profile*>(buffer);
-	return profile;
 }
