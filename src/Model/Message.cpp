b3eb73a6cd8c705eb659fa3530b4988d74934288
jihvijhojhviihogyuvi <wolfe.charlie.r@gmail.com>
2026-09-09 18:18:25 -0400

    Preserve read state when copying messages

--- src/Model/Message.cpp
+++ src/Model/Message.cpp
@@ -12,7 +12,7 @@ Message::Message(uint8_t picIndex) {
 	setPic(picIndex);
 }
 
-Message::Message(const Message& other) : uid(other.uid), convo(other.convo), outgoing(other.outgoing), received(other.received){
+Message::Message(const Message& other) : uid(other.uid), convo(other.convo), outgoing(other.outgoing), received(other.received), read(other.read){
 	clear();
 
 	if(other.type == TEXT){
@@ -31,6 +31,7 @@ Message& Message::operator=(const Message& other){
 	convo = other.convo;
 	outgoing = other.outgoing;
 	received = other.received;
+	read = other.read;
 
 	clear();
 
